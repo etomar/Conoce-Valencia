0e44bb336aec781df209e66b55e527417f258a97
jaumesevi <jsegarra@iesfuentesanluis.org>
2017-05-02 11:56:42 +0200

    BDConnect.java implementada

--- src/ConexionDB/BDConnect.java
+++ src/ConexionDB/BDConnect.java
@@ -5,10 +5,35 @@
  */
 package ConexionDB;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Lliurex
  */
 public class BDConnect {
+    public static String conData = "jdbc:mysql://127.0.0.1:3306/conocevalencia?" +
+                                           "user=root&password=";
     
+    public static Connection connect(){
+        
+        try {
+            Connection conn = DriverManager.getConnection(conData);
+            
+            return conn;
+            
+        } catch (SQLException ex) {
+                showMYSQLerrors(ex);
+        }
+        
+        return null;
+    }
+    
+    public static void showMYSQLerrors(SQLException ex){        
+        System.out.println("SQLException: " + ex.getMessage());
+        System.out.println("SQLState: " + ex.getSQLState());
+        System.out.println("VendorError: " + ex.getErrorCode());
+    }
 }
