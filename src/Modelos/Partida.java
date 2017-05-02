335970892f0afd8f14782e79c14e0383618c28f8
Lliurex <Lliurex@192.168.100.52>
2017-05-02 12:38:13 +0200

    Añdidos metodos de la clase

--- src/Modelos/Partida.java
+++ src/Modelos/Partida.java
@@ -5,10 +5,60 @@
  */
 package Modelos;
 
+import java.util.Calendar;
+import ConexionDB.*;
+
 /**
  *
  * @author Lliurex
  */
 public class Partida {
-    
+
+    private Calendar fecha;
+    private PreguntaGrupo[] preguntas;
+    private Grupo g;
+
+    public Partida() {
+    }
+
+    public Partida(Grupo g) {
+        preguntas = new PreguntaGrupo[10];
+        fecha = Calendar.getInstance();
+        this.g = g;
+    }
+
+    public PreguntaGrupo[] cargarPreguntas() {
+        ArrayList<Pregunta> al = PreguntaDAO.loadAll();
+        for (int i = 0; i < 10; i++) {
+            Pregunta p = al.get(Math.random()*al.size +1);
+            preguntas[i]=(PreguntaGrupo)p;
+        }
+        
+        return preguntas;
+    }
+
+    public Calendar getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Calendar fecha) {
+        this.fecha = fecha;
+    }
+
+    public PreguntaGrupo[] getPreguntas() {
+        return preguntas;
+    }
+
+    public void setPreguntas(PreguntaGrupo[] preguntas) {
+        this.preguntas = preguntas;
+    }
+
+    public Grupo getG() {
+        return g;
+    }
+
+    public void setG(Grupo g) {
+        this.g = g;
+    }
+
 }
