73140b2fb6ab24a7323fc77ca3c5dc0b6a2aa7fa
jaumesevi <jsegarra@iesfuentesanluis.org>
2017-05-02 12:16:29 +0200

    Modelos/Pregunta.java v0.1

--- src/Modelos/Pregunta.java
+++ src/Modelos/Pregunta.java
@@ -10,5 +10,57 @@ package Modelos;
  * @author Lliurex
  */
 public class Pregunta {
+    private int codigo;
+    private String enunciado;
+    private String[] respuestas;
+    private int respuesta_correcta;
+    private int veces;
     
+    public Pregunta(int codigo, String enunciado, String[] respuestas, int respuesta_correcta, int veces){
+        this.codigo=codigo;
+        this.enunciado=enunciado;
+        this.respuestas=respuestas;
+        this.respuesta_correcta=respuesta_correcta;
+        this.veces=veces;
+    }
+    
+    public int getCodigo(){
+        return codigo;
+    }
+
+    public String getEnunciado() {
+        return enunciado;
+    }
+
+    public String[] getRespuestas() {
+        return respuestas;
+    }
+
+    public int getRespuesta_correcta() {
+        return respuesta_correcta;
+    }
+
+    public int getVeces() {
+        return veces;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setEnunciado(String enunciado) {
+        this.enunciado = enunciado;
+    }
+
+    public void setRespuestas(String[] respuestas) {
+        this.respuestas = respuestas;
+    }
+
+    public void setRespuesta_correcta(int respuesta_correcta) {
+        this.respuesta_correcta = respuesta_correcta;
+    }
+
+    public void setVeces(int veces) {
+        this.veces = veces;
+    }
 }
