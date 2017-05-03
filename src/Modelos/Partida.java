b8a8cfd174497c683effa790cdb4baf3d9523955
Lliurex <Lliurex@192.168.100.52>
2017-05-03 13:52:29 +0200

    añadida duracion sesion

--- src/Modelos/Partida.java
+++ src/Modelos/Partida.java
@@ -17,11 +17,14 @@ import java.util.ArrayList;
  * @version 1.0
  */
 public class Partida {
+
     private int codigo;
     private int respuestas_correctas;
     private Calendar fecha;
     private PreguntaGrupo[] preguntas;
     private Grupo g;
+    private Calendar fin;
+    private String duracion;
 
     public Partida(Grupo g) {
         preguntas = new PreguntaGrupo[10];
@@ -33,21 +36,34 @@ public class Partida {
     public PreguntaGrupo[] cargarPreguntas() {
         ArrayList<Pregunta> al = PreguntaDAO.loadAll();
         for (int i = 0; i < 10; i++) {
-            Pregunta p = al.get((int)Math.random()*al.size()+1);
-            preguntas[i]=(PreguntaGrupo)p;
-            PreguntaDAO.sumarVeces(p);
+            Pregunta p = al.get((int) Math.random() * al.size() + 1);
+            preguntas[i] = (PreguntaGrupo) p;
         }
-        
+
         return preguntas;
     }
-    
+
+    public String DevolverDuracion(Calendar fecha, Calendar fin) {
+        fin = Calendar.getInstance();
+        int h = fecha.get(Calendar.HOUR_OF_DAY) - fin.get(Calendar.HOUR_OF_DAY);
+        int m = fecha.get(Calendar.MINUTE) - fin.get(Calendar.MINUTE);
+        int s = fecha.get(Calendar.SECOND) - fin.get(Calendar.SECOND);
+        String hora = Integer.toString(h);
+        String min = Integer.toString(m);
+        String sec = Integer.toString(s);
+        String d = hora + ":" + min + ":" + sec;
+
+        return d;
+    }
+
     /*
         Registrar los resultados de la partida
-    */
-    public void finalizar(){
+     */
+    public void finalizar() {
+        duracion = DevolverDuracion(this.fecha, this.fin);
         PartidaDAO.save(this);
     }
-    
+
     public Calendar getFecha() {
         return fecha;
     }
@@ -63,7 +79,7 @@ public class Partida {
     public int getRespuestas_correctas() {
         return respuestas_correctas;
     }
-    
+
     public void setPreguntas(PreguntaGrupo[] preguntas) {
         this.preguntas = preguntas;
     }
@@ -87,9 +103,13 @@ public class Partida {
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
-    
-    public void incrementarRespuestasCorrectas(){
-        this.respuestas_correctas++;
+
+    public Calendar getFin() {
+        return fin;
     }
-    
+
+    public void setFin(Calendar fin) {
+        this.fin = fin;
+    }
+
 }
